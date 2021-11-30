107cdc15b74ecbcfb189995e7a93c01995e4e774
이주현 <84003327+leeeju@users.noreply.github.com>
2021-11-30 10:04:03 +0900

    Update stop_tb3.py

--- ar_track/ar_track/script/stop_tb3.py
+++ ar_track/ar_track/script/stop_tb3.py
@@ -9,6 +9,7 @@ class StopTB3(Node): # 비상정지 노드
         super().__init__('remote_tb3_lift')
         qos_profile   = QoSProfile(depth=10)
         self.pub_tw   = self.create_publisher(Twist, '/cmd_vel', qos_profile)
+        # create_timer 1초마가 주기적으로 값을 생성해서 인위적으로 마커id 1을 찾았다는 착각을 주게 해서 로봇을 강제로 멈춤
         self.timer    = self.create_timer(1, self.stop_tb3_)
         
     def stop_tb3_(self):
